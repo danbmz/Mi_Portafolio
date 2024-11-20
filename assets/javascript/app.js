69691576b4cdd72def9a9f378201c78f0ac95828
danbmz <danbmz007@gmail.com>
2024-11-20 15:07:12 -0600

    Portafolio Listo para subir a Produccion

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,13 @@
 const formulario = document.querySelector('.formulario');
 const agradecimientoDiv = document.querySelector('.verificado__container');
+const titulo_contacto = document.querySelector('.contacto__titulo');
+const descripcion = document.querySelector('.contacto__descripcion');
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault(); // Evita el comportamiento predeterminado del formulario
     // Oculta el formulario y muestra el mensaje de agradecimiento
     formulario.style.display = 'none';
+    titulo_contacto.style.display = 'none';
+    descripcion.style.display = 'none';
     agradecimientoDiv.style.display = 'flex';
 });
